ef528e2fc4de767461015c7712da7a7fe4b85388
heytitle <pat.chormai@gmail.com>
2019-08-24 11:29:49 +0200

    add evaluation.py

---
+++ attacut/evaluation.py
@@ -0,0 +1,35 @@
+from collections import namedtuple
+from typing import Dict
+
+import numpy as np
+
+from nptyping import Array
+
+
+EvaluationMetrics = namedtuple(
+    "EvaluationMetrics",
+    ["tp", "fp", "fn", "precision", "recall", "f1"]
+)
+
+def compute_metrics(
+    labels: Array[np.int32],
+    preds: Array[np.int32]
+) -> EvaluationMetrics:
+
+    # manually implemented due to keep no. of dependencies minimal
+    tp = np.sum(preds * labels)
+    fp = np.sum(preds * (1-labels))
+    fn = np.sum((1-preds) * labels)
+
+    precision = tp / (tp+fp)
+    recall = tp / (tp+fn)
+    f1 = 2 * precision * recall / (precision + recall)
+
+    return EvaluationMetrics(
+        tp=tp,
+        fp=fp,
+        fn=fn,
+        precision=precision,
+        recall=recall,
+        f1=f1
+    )
---
+++ tests/evaluation.py
@@ -0,0 +1,25 @@
+import pytest
+
+import numpy as np
+
+from attacut import evaluation
+
+@pytest.mark.parametrize(
+    ("labels", "preds", "expected"),
+    [
+        # labels, preds, (tp, fp, fn)
+        ( [1, 1, 0], [1, 1, 1], (2, 1, 0) ),
+        ( [1, 1, 1], [1, 1, 0], (2, 0, 1) )
+    ]
+)
+def test_something(labels, preds, expected):
+    labels = np.array(labels)
+    preds = np.array(preds)
+
+    metrics = evaluation.compute_metrics(labels, preds)
+    actual = (metrics.tp, metrics.fp, metrics.fn)
+
+    np.testing.assert_array_equal(
+        actual,
+        expected
+    )
