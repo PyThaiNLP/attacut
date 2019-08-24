f663bac8678f7ce969e03095c1d81b8e118f5bc1
heytitle <pat.chormai@gmail.com>
2019-08-25 01:11:48 +0200

    resolve artifact path

--- attacut/artifacts/__init__.py
+++ attacut/artifacts/__init__.py
@@ -2,6 +2,9 @@ import os
 
 artifact_dir = os.path.dirname(__file__)
 
-def get_path(name):
-    # todo: check if the artifact exits
-    return f"{artifact_dir}/{name}"
+def get_path(name:str) -> str:
+    if name in ['attacut-c', 'attacut-sc']:
+        return f"{artifact_dir}/{name}"
+    else:
+        # if name isn't in the list, then it's a custom model
+        return name
