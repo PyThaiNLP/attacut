cfec0f29e5e8e9076b0b1450c118e0eb7ac9de86
heytitle <pat.chormai@gmail.com>
2019-08-25 03:31:19 +0200

    Bump version: 0.3 → 0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("./requirements.txt", "r") as f:
 
 setup(
    name="attacut",
-   version="0.3",
+   version="0.4",
    description="A useful module",
    author="Man Foo",
    author_email="foomail@foo.com",
