aa07133cea457a5fdedba532e3558455acb96450
heytitle <pat.chormai@gmail.com>
2019-08-25 23:42:26 +0200

    add long description

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 with open("./requirements.txt", "r") as f:
     requirements = list(map(lambda x: x.strip(), f.readlines()))
 
@@ -13,4 +16,11 @@ setup(
     install_requires=requirements,
     scripts=["scripts/attacut-cli"],
     package_data={"attacut": ["artifacts/**/*"]},
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
