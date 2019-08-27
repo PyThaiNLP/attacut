d6d63aca5d2bc01f22333ddb2bf9389f6641f921
heytitle <pat.chormai@gmail.com>
2019-08-27 16:26:37 +0200

    add logger

--- attacut/artifacts/__init__.py
+++ attacut/artifacts/__init__.py
@@ -1,5 +1,9 @@
 import os
 
+from attacut import logger
+
+log = logger.get_logger(__name__)
+
 artifact_dir = os.path.dirname(__file__)
 
 def get_path(name:str) -> str:
@@ -7,4 +11,5 @@ def get_path(name:str) -> str:
         return f"{artifact_dir}/{name}"
     else:
         # if name isn't in the list, then it's a custom model
+        log.info("model_path: %s" % name)
         return name
