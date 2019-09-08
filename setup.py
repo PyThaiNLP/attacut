954ad83fa92e43f1509eb8f631f8e5dc5d197781
heytitle <pat.chormai@gmail.com>
2019-09-08 09:26:37 +0200

    Bump version: 1.0.1 → 1.0.2-dev

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("./requirements.txt", "r") as f:
 
 setup(
     name="attacut",
-    version="1.0.1",
+    version="1.0.2-dev",
     description="Fast and Reasonably Accurate Word Tokenizer for Thai",
     author="Pattarawat Chormai et al.",
     author_email="foomail@foo.com",
