90052f844ffe04b7323c84b5887880a03d2c5289
heytitle <pat.chormai@gmail.com>
2019-10-18 19:50:16 +0200

    Bump version: 1.0.4 → 1.0.5-dev

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="attacut",
-    version="1.0.4",
+    version="1.0.5-dev",
     description="Fast and Reasonably Accurate Word Tokenizer for Thai",
     url="https://github.com/PyThaiNLP/attacut",
     author="Pattarawat Chormai et al.",
